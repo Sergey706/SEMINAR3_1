f8204426a3bee376c1dcd400265ea6cb46f189cb
sergey ogurtsov <sergey.ogurtsov@gmail.com>
2024-07-23 19:34:38 +0300

    решили задачу 3

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -132,3 +132,35 @@ public class Answer {
         System.Console.WriteLine($"{result}");
     }
 }*/
+//Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+
+using System;
+
+class Answer {
+    static void CheckIfEven(int number)
+    {
+      // Введите свое решение ниже
+        if (number % 2 == 0)
+        {
+            System.Console.WriteLine($"четное");
+        }
+        else System.Console.WriteLine($"нечетное");
+
+    }
+  
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = -8;
+        }
+
+        // Не удаляйте строки ниже
+        CheckIfEven(number);
+    }
+}
--- Lesson1/obj/Debug/net8.0/Lesson1.AssemblyInfo.cs
+++ Lesson1/obj/Debug/net8.0/Lesson1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c174ce0fb695128d8304465a211134f6f7263e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a55b9e134c742a8d0d0d798aa8cb5a72143549")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
