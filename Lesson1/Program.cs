187fca236f27154838dfed6ab5b0e0d503e30268
sergey ogurtsov <sergey.ogurtsov@gmail.com>
2024-07-23 19:59:27 +0300

    решили задачу 4

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -132,7 +132,7 @@ public class Answer {
         System.Console.WriteLine($"{result}");
     }
 }*/
-//Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+/*//Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
 
 using System;
 
@@ -162,5 +162,37 @@ class Answer {
 
         // Не удаляйте строки ниже
         CheckIfEven(number);
+    }
+}*/
+
+//Внутри класса Answer напишите метод PrintEvenNumbers, которая на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), после каждого числа должен быть знак пробела.
+using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 2 == 0)
+            {
+                System.Console.Write($"{i} ");
+            }
+        }
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 8;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
     }
 }
--- Lesson1/obj/Debug/net8.0/Lesson1.AssemblyInfo.cs
+++ Lesson1/obj/Debug/net8.0/Lesson1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a55b9e134c742a8d0d0d798aa8cb5a72143549")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8204426a3bee376c1dcd400265ea6cb46f189cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
